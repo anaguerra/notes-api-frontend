fb17e68a1954bee89610945dd2e45b301cb5061d
Ana Guerra <anamaria.guerra@icontainers.com>
2023-03-23 10:07:22 +0000

    estilos condicionales dependiendo del state

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,28 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // const LoginForm = ({handleLogin, handleUsernameChange, handlePasswordChange}) => {
 const LoginForm = (props) => {
-  return(
-    <form onSubmit={props.handleLogin}>
-      <div>
-      <input 
-        type='text'
-        value={props.username}
-        name='Username'
-        placeholder='Username'
-        onChange={props.handleUsernameChange}
-      />
+  const [loginVisible, setLoginVisible] = useState(false)
+
+  const hideWhenVisible = { display: loginVisible ? 'none' : ''}
+  const showWhenVisible = { display: loginVisible ? '' : 'none'}
+
+  return (
+    <div>
+      <div style={hideWhenVisible}>
+        <button onClick={() => setLoginVisible(true)}>Show login</button>
       </div>
-              
-      <input 
-        type='password'
-        value={props.password}
-        name='Password'
-        placeholder='Password'
-        onChange={props.handlePasswordChange}
-      />
-      <button>Login</button>
-    </form>
+
+      <div style={showWhenVisible}>
+        <form onSubmit={props.handleLogin}>
+          <div>
+          <input 
+            type='text'
+            value={props.username}
+            name='Username'
+            placeholder='Username'
+            onChange={props.handleUsernameChange}
+          />
+          </div>
+                  
+          <input 
+            type='password'
+            value={props.password}
+            name='Password'
+            placeholder='Password'
+            onChange={props.handlePasswordChange}
+          />
+          <button>Login</button>
+        </form>
+        <button onClick={() => setLoginVisible(false)}>Cancel</button>
+      </div>
+    </div>
   )
 }
 
